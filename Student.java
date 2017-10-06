6739809c7af5909dd7483d129c15e7b129124d48
NathanDoher <32580897+NathanDoher@users.noreply.github.com>
2017-10-06 17:14:41 -0500

    Delete Student.java

--- Student.java
+++
@@ -1,9 +0,0 @@
-
-public class Student extends Customer{
-
-	public Student(String customerCode, Person contactPerson, String name, Address address) {
-		super(customerCode, contactPerson, name, address);
-		// TODO Auto-generated constructor stub
-	}
-	
-}
