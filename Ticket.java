e04854cff82f6e8e93a6a16a3552704ad04374b1
NathanDoher <32580897+NathanDoher@users.noreply.github.com>
2017-10-06 17:14:30 -0500

    Delete Ticket.java

--- Ticket.java
+++
@@ -1,8 +0,0 @@
-//movie ticket and season pass
-public abstract class Ticket extends Product{
-
-	public Ticket(String productCode, String productType) {
-		super(productCode, productType);
-	}
-	//tax rate calculation
-}
