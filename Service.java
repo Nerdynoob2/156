d20a7cb001fc7604fd3a5c4cea6bada14b759593
NathanDoher <32580897+NathanDoher@users.noreply.github.com>
2017-10-06 17:14:48 -0500

    Delete Service.java

--- Service.java
+++
@@ -1,8 +0,0 @@
-//parkingpass and refreshments
-public abstract class Service extends Product{
-
-	public Service(String productCode, String productType) {
-		super(productCode, productType);
-	}
-	//tax rate calculation
-}
