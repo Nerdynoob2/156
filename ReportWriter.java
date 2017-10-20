7a86ce6114182c4a0115185ff0959eb970e01912
Nerdynoob2 <32580931+Nerdynoob2@users.noreply.github.com>
2017-10-20 01:34:13 -0600

    Delete ReportWriter.java

--- ReportWriter.java
+++
@@ -1,7 +0,0 @@
-
-public class ReportWriter {
-	//TODO add method for writing summary report (take list of invoices as argument)
-	
-	//TODO add method for writing detailed report (take single invoice as argument)
-	// **OR** take list of invoices as argument and print detailed report for each
-}
