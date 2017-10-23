2e1147236c9da9edfbdba40319a5843c033afe5f
NathanDoher <32580897+NathanDoher@users.noreply.github.com>
2017-10-23 11:28:26 -0500

    Add files via upload

--- invoiceDB.sql
+++ invoiceDB.sql
@@ -22,14 +22,84 @@ ZipCode varchar (15)
 create table Address(
 AddressID int not null auto_increment,
 primary key (AddressID),
-
 CityZipID INT NULL,
-foreign key (CityZipID) references CityZipID(CityZipID),
-
+foreign key (CityZipID) references CityZip(CityZipID),
 StateID INT NULL,
-foreign key (StateID) references StateID(StateID),
-
-Country INT NULL,
-foreign key (Country) references Country(Country)
+foreign key (StateID) references State(StateID),
+CountryID INT NULL,
+foreign key (CountryID) references Country(CountryID)
 );
 
+create table Persons(
+PersonID int not null auto_increment,
+primary key (PersonID),
+PersonCode varchar (10),
+FirstName varchar (50),
+LastName varchar (50),
+AddressID INT NULL,
+foreign key (AddressID) references Address(AddressID)
+);
+
+create table Email(
+EmailID int not null auto_increment,
+primary key (EmailID),
+Email varchar (250),
+PersonID INT NULL,
+foreign key (PersonID) references Persons(PersonID)
+);
+
+create table Customers(
+CustomerID int not null auto_increment,
+primary key (CustomerID),
+CustomerCode varchar (10),
+CustomerType varchar (1),
+ContactPersonID INT NULL,
+foreign key (ContactPersonID) references Persons(PersonID),
+CompanyName varchar (50),
+AddressID INT NULL,
+foreign key (AddressID) references Address(AddressID)
+);
+
+create table CustomerPersons(
+CustomerPersonsID int not null auto_increment,
+primary key (CustomerPersonsID),
+CustomerID INT NULL,
+foreign key (CustomerID) references Customers(CustomerID),
+PersonID INT NULL,
+foreign key (PersonID) references Persons(PersonID)
+);
+
+#---------------------------
+#Products UNDER CONSTRUCTION
+#---------------------------
+
+create table ProductOrder(
+ProductOrderID int not null auto_increment,
+primary key (ProductOrderID),
+ProductCode varchar (10),
+
+foreign key (CustomerID) references Customers(CustomerID),
+ProductCode varchar (1)
+);
+
+create table MovieTickets(
+MovieTicketsID int not null auto_increment,
+primary key (MovieTicketsID),
+MovieCode varchar (10),
+DateTime datetime,
+MovieTitle varchar (50),
+AddressID INT NULL,
+foreign key (AddressID) references Address(AddressID),
+Screen varchar (2),
+Cost 
+);
+
+create table ParkingPass(
+ParkingPassID int not null auto_increment,
+primary key (ParkingPassID),
+ParkingPassCode varchar (10),
+DateTime datetime,
+MovieTitle varchar (50),
+AddressID INT NULL,
+foreign key (AddressID) references Address(AddressID)
+);
