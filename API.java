648d4dc6098b38a9b7a19533bdd6548a134ae0b0
NathanDoher <32580897+NathanDoher@users.noreply.github.com>
2017-11-12 14:40:06 -0600

    Add files via upload

---
+++ API.java
@@ -0,0 +1,44 @@
+	
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+	
+public class API {
+
+		public static final String url = "jdbc:mysql://cse.unl.edu/ndoher";
+		public static final String username = "ndoher";
+		public static final String password = "Echo007!";
+		
+		static public Connection getConnection()
+		{
+			try {
+				Class.forName("com.mysql.jdbc.Driver").newInstance();
+			} catch (InstantiationException e) {
+				System.out.println("InstantiationException: ");
+				e.printStackTrace();
+				throw new RuntimeException(e);
+			} catch (IllegalAccessException e) {
+				System.out.println("IllegalAccessException: ");
+				e.printStackTrace();
+				throw new RuntimeException(e);
+			} catch (ClassNotFoundException e) {
+				System.out.println("ClassNotFoundException: ");
+				e.printStackTrace();
+				throw new RuntimeException(e);
+			}
+			
+			Connection conn = null;
+
+			try {
+				conn = DriverManager.getConnection(API.url, API.username, API.password);
+			} catch (SQLException e) {
+				System.out.println("SQLException: ");
+				e.printStackTrace();
+				throw new RuntimeException(e);
+			}
+			
+			return conn;
+		}
+	
+	
+}
